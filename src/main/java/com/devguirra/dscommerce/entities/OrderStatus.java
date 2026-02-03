78d532e09e58b3b9b0ba04b845672a02cd44e941
daniloguirra <daniloguirra@hotmail.com>
2026-02-03 17:45:42 -0300

    DscommercerApplication initial

---
+++ src/main/java/com/devguirra/dscommerce/entities/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.devguirra.dscommerce.entities;
+
+public enum OrderStatus {
+    WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
+}
---
+++ src/test/java/com/devguirra/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devguirra.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
