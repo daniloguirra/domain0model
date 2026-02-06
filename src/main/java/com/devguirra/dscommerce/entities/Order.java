087356124e0e096d7e22cb42b1cb2c033310aaa5
daniloguirra <daniloguirra@hotmail.com>
2026-02-06 12:21:19 -0300

    Payment

--- src/main/java/com/devguirra/dscommerce/entities/Order.java
+++ src/main/java/com/devguirra/dscommerce/entities/Order.java
@@ -11,12 +11,19 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
     private OrderStatus status;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
 
     public Order(){
     }
